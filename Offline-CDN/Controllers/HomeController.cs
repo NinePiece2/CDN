5a5e3fd15ad34cc73854a6d5f56bc0c5a0a005d9
NinePiece2 <romit.sagu@hotmail.com>
2024-04-19 20:36:25 -0400

    Update

--- Offline-CDN/Controllers/HomeController.cs
+++ Offline-CDN/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace Offline_CDN.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Offline()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
