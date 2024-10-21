1b40670ea1d727f32180175f9b17da674710a556
NinePiece2 <romit.sagu@hotmail.com>
2024-10-20 23:12:08 -0400

    Initial New CDN

---
+++ CDN/CDN/Data/Tables/CDNUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CDN.Data.Tables;
+
+public class CDNUser : IdentityUser
+{
+    [MaxLength(255)]
+    public string? Name { get; set; }
+    public bool AccountApproved { get; set; }
+}
+
--- Offline-CDN/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Offline_CDN.MvcApplication" Language="C#" %>
